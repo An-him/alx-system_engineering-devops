d53d59886e5e14d5211b581c04b6e2eb92b3f955
Muhwezi <michealphenh10@gmail.com>
2024-06-28 22:05:02 +0300

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+# Creates a file called school with I LOVE PUPPET as the content
 file { '/tmp/school':
 content => 'I love Puppet',
-group   => 'www-data',
-owner   => 'www-data',
+group   => "www-data",
+owner   => "www-data",
 mode    => '0744',
 }
